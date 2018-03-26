e98718e211bcd253758e2b24ad95c5b7dc5a2f27
gilangegi <gilangpratamaw@gmail.com>
2018-03-26 12:10:33 +0700

    tes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-
-const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
-};
-
-
-
-const App = () => (
-  <div style={styles}>
-    <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {'\u2728'}</h2>
-  </div>
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ApiHelper from "./helper/ApiHelper";
+import App from "./App";
+ApiHelper.getJudul();
+ReactDOM.render(
+  <App />,
+  document.documentElement.parentNode
 );
-
-render(<App />, document.getElementById('root'));
